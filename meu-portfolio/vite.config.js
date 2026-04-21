181e6bca1be49d9422fd04cdf9e262f996d0c467
Davi <fdavi07@gmail.com>
2026-04-21 15:23:07 -0300

    pakcage-lock.json atualizado com 2 scripts

--- meu-portfolio/vite.config.js
+++ meu-portfolio/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/meu-portfolio/', // Nome EXATO do seu repositório no GitHub
+  base: '/Portfolio/', // Nome EXATO do seu repositório no GitHub
 })
