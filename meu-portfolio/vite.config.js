453938142b20939581e8a1d65377ac7e9f1b628c
davi <fdavi07@gmail.com>
2026-04-29 21:19:27 -0300

    agora foi

--- meu-portfolio/vite.config.js
+++ meu-portfolio/vite.config.js
@@ -1,11 +1,10 @@
-﻿// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/react-swc'
-import path from 'path'
+﻿import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path' // Você pode precisar importar o 'path' do Node
 
 export default defineConfig({
   plugins: [react()],
-  base: './', 
+  base: "/Portfolio/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
